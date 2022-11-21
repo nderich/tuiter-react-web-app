89bdfa1842dc7a4882d88f51b1deb25bfd9e94e6
Nicolas <nicolas@Nicolass-MacBook-Pro-2.local>
2022-11-20 21:18:23 -0500

    like

--- src/tuiter/tuits/tuit-stats.js
+++ src/tuiter/tuits/tuit-stats.js
@@ -1,4 +1,6 @@
 import React from "react";
+import {updateTuitThunk} from "../../services/tuits-thunks";
+import {useDispatch} from "react-redux";
 
 const TuitStats = (
     {
@@ -6,6 +8,8 @@ const TuitStats = (
 
     }
 ) => {
+    const dispatch = useDispatch();
+
     return(
 
         <div className="row text-muted mt-2">
@@ -27,7 +31,10 @@ const TuitStats = (
                 {
                     tuits.liked &&
                     <span className="col-3">
-                     <i className="bi bi-heart-fill text-danger"></i> {tuits.likes}
+                     <i onClick={() => dispatch(updateTuitThunk({
+                         ...tuits,
+                         likes: tuits.likes + 1
+                     }))} className="bi bi-heart-fill me-2 text-danger"></i> {tuits.likes}
                     </span>
                 }
 
